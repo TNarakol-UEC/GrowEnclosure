90cc5f55040dc629c48763ecd4b7aaf792274425
TNarakol <TNarakol@ulnooweg.ca>
2024-05-15 11:36:49 -0300

    started main.py deleted lcddisp.py that is deprecated by seyit

---
+++ Software/Python/V1.0/code/main.py
@@ -0,0 +1,13 @@
+#Ulnooweg Education Centre - ulnoowegeducation.ca
+#Growth enclosure V1.0
+#All rights reserved
+#
+#This function is the main function
+#This functions should call other submodules/functions and execute them as needed/scheduled
+#This functions always run under grobot service
+#
+##############################################
+#MODULE IMPORTS
+import asyncio
+
+##############################################
